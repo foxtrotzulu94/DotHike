bbc876f9833fb71af2ddcc9d3873fec481c4cf6f
Javier E. Fajardo <j_fajard@encs.concordia.ca>
2015-10-22 19:43:20 -0400

    Reverted Master to Previous Status

--- app/src/main/java/me/dotteam/dotprod/IntroActivity.java
+++ app/src/main/java/me/dotteam/dotprod/IntroActivity.java
@@ -1,32 +1,16 @@
 package me.dotteam.dotprod;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class IntroActivity extends AppCompatActivity {
-
-    Button mapsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-
-        mapsButton = (Button) findViewById(R.id.mapsButton);
-
-        mapsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent mapsIntent = new Intent(IntroActivity.this, MapsActivity.class);
-                startActivity(mapsIntent);
-            }
-        });
-
     }
 
     @Override
