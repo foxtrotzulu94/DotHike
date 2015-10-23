50a3afd8f6bf2bf45be3a91896b3a75e266cb17d
CoryB <cory.bonneau2424@gmail.com>
2015-10-23 19:24:17 -0400

    All activities are connected

--- app/src/main/java/me/dotteam/dotprod/EnvCondActivity.java
+++ app/src/main/java/me/dotteam/dotprod/EnvCondActivity.java
@@ -1,16 +1,28 @@
 package me.dotteam.dotprod;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class EnvCondActivity extends AppCompatActivity {
-
+    private Button buttonBackToMainHike;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_env_cond);
+        buttonBackToMainHike = (Button) findViewById(R.id.buttonBack);
+
+        buttonBackToMainHike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMainHike = new Intent(EnvCondActivity.this, MainHikeActivity.class);
+                startActivity(intentMainHike);
+            }
+        });
     }
 
     @Override
