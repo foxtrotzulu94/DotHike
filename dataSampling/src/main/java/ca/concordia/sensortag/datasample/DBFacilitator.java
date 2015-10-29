3c814f72f03cbcec0635aa3c9e7d1c769d8b6db0
Eric Tremblay <e.tremblay18@hotmail.com>
2015-10-29 08:58:00 -0400

    Created sqlite query to create two tables: one for timestamps and the other for the parameters of the recording.

--- dataSampling/src/main/java/ca/concordia/sensortag/datasample/DBFacilitator.java
+++ dataSampling/src/main/java/ca/concordia/sensortag/datasample/DBFacilitator.java
@@ -11,9 +11,23 @@ import android.util.Log;
 public class DBFacilitator extends SQLiteOpenHelper {
 
     public static final String DB_NAME="DataSamples";
-    public static final String DB_TABLE_NAME="sample_set";
-    public static final String SQL_CREATION="";
-    public static final String SQL_DESTROY="DROP TABLE IF EXIST"+DB_TABLE_NAME;
+    public static final String DB_TABLE1_NAME="sample_set";
+    public static final String TABLE1_COLUMN_ID="_id";
+    public static final String TABLE1_COLUMN_NAME="timestamp";
+    public static final String DB_TABLE2_NAME="parameters";
+    public static final String TABLE2_COLUMN1_NAME="status";
+    public static final String TABLE2_COLUMN2_NAME="rec_duration";
+    public static final String TABLE2_COLUMN3_NAME="rec_samples";
+    public static final String TABLE2_COLUMN4_NAME="time_elapsed_ms";
+    public static final String SQL_CREATION="CREATE TABLE "
+            + DB_TABLE1_NAME + "(" + TABLE1_COLUMN_ID
+            + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + TABLE1_COLUMN_NAME + " REAL);"
+            + " CREATE TABLE " + DB_TABLE2_NAME + "("
+            + TABLE2_COLUMN1_NAME + " TEXT, " + TABLE2_COLUMN2_NAME
+            + " REAL, " + TABLE2_COLUMN3_NAME + " INT, "
+            + TABLE2_COLUMN4_NAME + " REAL);";
+    public static final String SQL_DESTROY="DROP TABLE IF EXIST"+ DB_TABLE1_NAME + ", " + DB_TABLE2_NAME;
     public static final int DB_VERSION = 1;
 
     protected Context callerContext;
