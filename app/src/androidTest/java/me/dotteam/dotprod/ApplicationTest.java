837c5afdd3ecb94713e3e62b45db531b42c38ce6
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-11-18 01:38:11 -0500

    Issue #51: Initial Testing setup

--- app/src/androidTest/java/me/dotteam/dotprod/ApplicationTest.java
+++ app/src/androidTest/java/me/dotteam/dotprod/ApplicationTest.java
@@ -1,13 +1,31 @@
 package me.dotteam.dotprod;
 
 import android.app.Application;
+import android.content.pm.PackageInfo;
 import android.test.ApplicationTestCase;
+import android.test.MoreAsserts;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    private Application application;
+
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        createApplication();
+        application = getApplication();
+
+    }
+
+    public void testCorrectVersion() throws Exception {
+        PackageInfo info = application.getPackageManager().getPackageInfo(application.getPackageName(), 0);
+        assertNotNull(info);
+        MoreAsserts.assertMatchesRegex("\\d\\.\\d", info.versionName);
     }
 }
