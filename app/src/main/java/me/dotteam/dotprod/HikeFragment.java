19ba117d6a08c8f1cacd46012619205a6f054d6e
Javier E. Fajardo <foxtrotzulu94@gmail.com>
2015-12-01 16:57:03 -0500

    Issue #65: Extracted the CompassAnimator

--- app/src/main/java/me/dotteam/dotprod/HikeFragment.java
+++ app/src/main/java/me/dotteam/dotprod/HikeFragment.java
@@ -27,63 +27,63 @@ import java.util.Random;
  */
 public class HikeFragment extends Fragment implements OnMapReadyCallback {
 
-    private class CompassAnimator extends Thread{
-
-        float currentDegrees = 0.0f;
-        float finalDegrees = 0.0f;
-        float dampingPercentage = 0.05f;
-        boolean runningThread =false;
-
-        @Override
-        public void run(){
-            runningThread =true;
-            while(runningThread){
-                if (currentDegrees!=finalDegrees){
-                    currentDegrees = lerp(currentDegrees,finalDegrees,dampingPercentage);
-                    updateUI(currentDegrees);
-                }
-                else{
-                    runningThread = false;
-                }
-
-                try{
-                    sleep(34); //30 FPS, no compass needs to be at 60...
-                }
-                catch (InterruptedException e){
-                    runningThread = false;
-                }
-
-            }
-        }
-
-        private float lerp(float start, float end, float percentage){
-            return start+( percentage*(end-start)  );
-        }
-
-        private void updateUI(final float value){
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    mCompassView.setDegrees(value);
-                }
-            });
-        }
-
-        public void setNewValue(float newValue){
-            finalDegrees = newValue % 360;
-            if(!isAlive() && !runningThread)
-                this.start();
-        }
-
-        public void setAndStop(float newValue){
-            updateUI(newValue);
-            runningThread = false;
-        }
-
-        public void stopAnimation(){
-            setAndStop(finalDegrees);
-        }
-    }
+//    private class CompassAnimator extends Thread{
+//
+//        float currentDegrees = 0.0f;
+//        float finalDegrees = 0.0f;
+//        float dampingPercentage = 0.05f;
+//        boolean runningThread =false;
+//
+//        @Override
+//        public void run(){
+//            runningThread =true;
+//            while(runningThread){
+//                if (currentDegrees!=finalDegrees){
+//                    currentDegrees = lerp(currentDegrees,finalDegrees,dampingPercentage);
+//                    updateUI(currentDegrees);
+//                }
+//                else{
+//                    runningThread = false;
+//                }
+//
+//                try{
+//                    sleep(34); //30 FPS, no compass needs to be at 60...
+//                }
+//                catch (InterruptedException e){
+//                    runningThread = false;
+//                }
+//
+//            }
+//        }
+//
+//        private float lerp(float start, float end, float percentage){
+//            return start+( percentage*(end-start)  );
+//        }
+//
+//        private void updateUI(final float value){
+//            getActivity().runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    mCompassView.setDegrees(value);
+//                }
+//            });
+//        }
+//
+//        public void setNewValue(float newValue){
+//            finalDegrees = newValue % 360;
+//            if(!isAlive() && !runningThread)
+//                this.start();
+//        }
+//
+//        public void setAndStop(float newValue){
+//            updateUI(newValue);
+//            runningThread = false;
+//        }
+//
+//        public void stopAnimation(){
+//            setAndStop(finalDegrees);
+//        }
+//    }
 
 
     private String TAG = "HikeFragment";
@@ -113,7 +113,7 @@ public class HikeFragment extends Fragment implements OnMapReadyCallback {
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString() + " must implement HikeFragmentListener");
         }
-        mCompassAnimator = new CompassAnimator();
+        mCompassAnimator = new CompassAnimator(getActivity(),mCompassView);
     }
 
     @Override
